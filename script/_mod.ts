0427a3f105a6b5cf5b5f1d7f5baedf2dda6f419b
Nexxkinn <2881204-Nexxkinn@users.noreply.gitlab.com>
2020-12-10 21:13:07 +0700

    add new hml support

--- script/_mod.ts
+++ script/_mod.ts
@@ -2,6 +2,7 @@ import * as hnx from './hnx.ts';
 import * as chk from './chk.ts';
 import * as exh from './exh.ts';
 import * as nhn from './nhn.ts';
+import * as hml from './hml.ts';
 import { de, en, DownMeta } from './_deps.ts';
 
 // TODO: fix these redudancies.
@@ -9,14 +10,16 @@ const srvc:any = {
     hnx, 
     chk,
     exh,
-    nhn
+    nhn,
+    hml
 }
 
 const dict:{ [key: string]: string } = {
     'kla642xlkh9ou0xerb9oo33k...':'kl9jf',
     'rlf641xlh1d6fnstkkfa43szby...':'bkyo6',
     'be4of1srrdf6asqzrtqf...':'be4of',
-    'k1you3qybhya43qhrf...':'k1yo4'
+    'k1you3qybhya43qhrf...':'k1yo4',
+    'kly9v33kkg9onnv...':'kl66n'
 }
 
 export const service = (key:string) => srvc?.[de(dict?.[key])];
---
+++ script/hml.ts
@@ -0,0 +1,79 @@
+import { de, grab, DownMeta, DownRequest, DownType, DownPagesRequest, PageRequest } from "./_deps.ts";
+
+const token  = de("kly9v33kkg9onnv...");
+const srvc   = de("kly9v33kkg9onnv...");
+const gid_rx = /([\d]+)\.html/g;
+
+export async function metadata(link: string): Promise<DownMeta> {
+    const [[,uid]]    = link.matchAll(gid_rx);
+    const g_i = await get_info(uid);
+
+    const title = g_i.title;
+    const length = g_i.files.length;
+
+    const files:any = new Array<{filename:string,url:string}>();
+
+    const get_subdomain = (hash:string) => {
+        let  num = parseInt(hash.slice(hash.length - 3, hash.length - 1),16);
+        const fe = num < 0x30 ? 2 : 3;
+             num = num < 0x09 ? 1 : num;
+        return String.fromCharCode(97 + (num % fe))
+    }
+
+    for( const {hash,name} of g_i.files) {
+        const sub = get_subdomain(hash);
+        const ext = name.split('.').pop();
+        const path = `${hash.slice(hash.length -1)}/${hash.slice(hash.length - 3, hash.length - 1)}`;
+        const url = `https://${sub}b.${token}/images/${path}/${hash}.${ext}`;
+        
+        files.push({filename:name,url});
+    }
+
+    const tn_file = () => {
+        const file = files[0];
+        const path = file.url.split('/images/').pop();
+        return `https://tn.${token}/smallbigtn/${path}`;
+    }
+
+    const thumbnail:DownRequest = {
+        input:tn_file(),
+        init:undefined,
+    }
+
+    const download = {
+        [Symbol.asyncIterator]() {
+            return {
+                async next() {
+                    if (files.length === 0) return Promise.resolve({ value: { init: undefined, input: '', filename: '' }, done: true });
+                    
+                    const { filename, url:input } = files.shift() || {};
+                    
+                    const download: PageRequest = {
+                        filename,
+                        input,
+                        init: { headers: { 'referer': link } }
+                    }
+                    return Promise.resolve({ value: download, done: false });
+                }
+            }
+        }
+    }
+
+    return { 
+        type:DownType.PAGES,
+        srvc,
+        title,
+        length,
+        uid,
+        download,
+        thumbnail
+    }
+}
+
+async function get_info(id:string){
+    const g_info_url = `https://ltn.${token}/galleries/${id}.js`;
+
+    const g_info_fetch = await fetch(g_info_url);
+    const g_info_res = await g_info_fetch.text();
+    return JSON.parse(g_info_res.split("galleryinfo = ")?.pop() || '');
+}
