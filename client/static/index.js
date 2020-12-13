463e31ea95f3156a7cd92d5889d0066ea30d1a31
Nexxkinn <2881204-Nexxkinn@users.noreply.gitlab.com>
2020-12-13 16:15:58 +0700

    implement icons for client UI

--- client/static/index.js
+++ client/static/index.js
@@ -189,13 +189,17 @@ function liblist_item(parent,args) {
     const rem = document.createElement('fast-button');
           rem.appearance = 'stealth';
           rem.className = 'option';
-          rem.append('remove');
+          rem.title = 'Remove this gallery'
+          rem.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>`
 
     const info = document.createElement('fast-button');
           info.appearance = 'stealth';
           info.className = 'option';
-          info.append('info');
-
+          info.title = 'View this gallery'
+          info.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-image"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>`
+          
     const name = document.createElement('div');
           name.title = title;
           name.className = 'title';
