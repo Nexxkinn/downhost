b726940978e495162089eef5385decefa1e8aba3
Nexxkinn <2881204-Nexxkinn@users.noreply.gitlab.com>
2020-12-25 23:00:45 +0700

    initial implementation for theme switching

--- client/static/index.js
+++ client/static/index.js
@@ -65,6 +65,8 @@ async function init() {
     const catalogPanel = document.getElementById('downpanel');
     const libraryPanel = document.getElementById('libpanel');
 
+    const provider     = document.getElementsByTagName('fast-design-system-provider')[0];
+    const theme_switch = document.getElementById('switch-theme');
 
     const parser    = new DOMParser()
     const rem_svg   = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>';
@@ -139,6 +141,17 @@ async function init() {
 
     field.valueChanged = (_) => {
         submit.hidden = !field.value.startsWith('http');
+    }
+
+    theme_switch.onclick = (_) => {
+        if(theme_switch.checked) {
+            document.documentElement.style.setProperty('--background-color', '#FFFFFF');
+            provider.backgroundColor = '#FFFFFF';
+        }
+        else {
+            document.documentElement.style.setProperty('--background-color', '#181818');
+            provider.backgroundColor = '#181818';
+        }
     }
 
     await refreshList();
