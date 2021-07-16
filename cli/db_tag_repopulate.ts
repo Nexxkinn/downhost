989fdcc4eec4d2fea95e47d3d3559b021edffb1c
Nexxkinn <2881204-Nexxkinn@users.noreply.gitlab.com>
2021-07-17 03:14:24 +0700

    fix db_tag_repopulate.ts

--- cli/db_tag_repopulate.ts
+++ cli/db_tag_repopulate.ts
@@ -2,17 +2,17 @@ import { DB } from "https://deno.land/x/sqlite@v2.4.2/mod.ts";
 import { resolve } from '../lib/script/_mod.ts';
 
 type DownTag = {
-    ns:string,
-    tag:string
+    ns: string,
+    tag: string
 }
-const log = (str:string) => console.log(str);
+const log = (str: string) => console.log(str);
 async function fetchTagstoDB() {
     log("DownHost DB Utility");
     log("Repopulate item tags for previously download items");
     log("WARNING: THIS WILL RESET ALL TAGS AVAILABLE IN YOUR DATABASE.");
     log("MAKE SURE YOU HAVE A COPY OF YOUR DATABASE FIRST.");
-    const HasRead = Deno.args.find( (v) => v === '--has-read' || '-v');
-    if(!HasRead) {
+    const HasRead = Deno.args.find((v) => v === '--has-read' || '-v');
+    if (!HasRead) {
         log("Rerun this app with --has-read args at the end if you've made a backup.")
         Deno.exit();
     }
@@ -20,7 +20,7 @@ async function fetchTagstoDB() {
     try {
         await Deno.stat('db.sqlite');
         // successful, file or directory must exist
-      } catch (error) {
+    } catch (error) {
         log('ERROR: Unable to find db.sqlite');
         log('Please run this utility on the same working directory as the one mentioned above.');
         Deno.exit()
@@ -28,32 +28,38 @@ async function fetchTagstoDB() {
 
     const db = new DB('db.sqlite');
     resetTagDB(db);
-    
+
     const query = db.query('SELECT hash,url from catalog');
 
-    let source:URL, hash:string, count=0, skip=0;
-    for( const [local_hash,url] of query ) {
-        if(!url) {
-            log(`skip ${local_hash}...(${skip+=1})`);
+    let source: URL, hash: string, count = 0, skip = 0;
+    for (const [local_hash, url] of query) {
+        if (!url) {
+            log(`skip ${local_hash}...(${skip += 1})`);
             continue;
         }
         source = new URL(url);
         hash = String(local_hash);
         log(`processing ${hash}`);
-        const metadata = await resolve(source);
-        const { srvc } = metadata;
-        let tags:DownTag[] = metadata.tags ?? [];
-        // add "source" tag
-        tags.filter((x) => x.ns.toLowerCase() !== "source");
-        tags.push({ns:"source",tag:srvc});
+        try {
+            const metadata = await resolve(source);
+            const { srvc } = metadata;
+            let tags: DownTag[] = metadata.tags ?? [];
+            // add "source" tag
+            tags.filter((x) => x.ns.toLowerCase() !== "source");
+            tags.push({ ns: "source", tag: srvc });
 
-        // insert tags into the table
-        for ( const {ns,tag} of tags) {
-            db.query(`INSERT OR IGNORE INTO tagrepo(ns, tag) VALUES(?, ?)`,[ns,tag]);
-            const [[id]] = db.query (`SELECT id from tagrepo WHERE ns=? AND tag=? LIMIT 1`,[ns,tag]);
-            db.query(`INSERT OR IGNORE INTO tag(hash, tag_id) VALUES(?, ?)`,[hash,id]);
+            // insert tags into the table
+            for (const { ns, tag } of tags) {
+                db.query(`INSERT OR IGNORE INTO tagrepo(ns, tag) VALUES(?, ?)`, [ns, tag]);
+                const [[id]] = db.query(`SELECT id from tagrepo WHERE ns=? AND tag=? LIMIT 1`, [ns, tag]);
+                db.query(`INSERT OR IGNORE INTO tag(hash, tag_id) VALUES(?, ?)`, [hash, id]);
+            }
+            
+            log(`done...(${count += 1})`)
         }
-        log(`done...(${count+=1})`)
+        catch (e) {
+            log(`error: ${e.message}. skipping...(${skip += 1})`)
+        }
     }
 
     log(`${count} items has been processed, ${skip} skipped.`);
@@ -61,7 +67,7 @@ async function fetchTagstoDB() {
 
 await fetchTagstoDB();
 
-function resetTagDB(db:DB) {
+function resetTagDB(db: DB) {
     db.query('DELETE FROM tag');
     db.query('DELETE FROM tagrepo');
     db.query('VACUUM');
